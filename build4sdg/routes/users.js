3f3ebffc6df5fda1b949417a6b9d4c853f237fcf
Kalungez <muthomititus+github@gmail.com>
2020-05-21 13:13:32 +0300

    add route for users

--- build4sdg/routes/users.js
+++ build4sdg/routes/users.js
@@ -7,7 +7,13 @@ const {
 	deleteUser,
 } = require('../controllers/users');
 
+const User = require('../models/User');
+
 const router = express.Router();
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+router.use(authorize('admin'));
 
 router.route('/').get(getUsers).post(createUser);
 router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
